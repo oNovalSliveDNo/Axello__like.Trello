f11a1a1205a2780dba07ad5c35b33ef148a2ac57
oNovlaSliveDNo <novosel.dan@gmail.com>
2023-06-11 16:28:30 +1000

    324234234

--- cl_board.py
+++ cl_board.py
@@ -2,35 +2,57 @@ from tkinter import *
 
 
 class Board():
-    def __init__(self, name:str, btype:str):
+    def __init__(self, owner:str, name: str, btype: str, start_x:int, width_of_column:int):
         self.name = name
         self.type = btype
-        self.columns = []
-        self.owner = None
-
-    def create_board_menu_widgets(self, start_x:int):
         self.start_x = start_x
+        self.width_of_column = width_of_column
+        self.owner = owner
+        self.columns = []
 
+        self.new_list_bttn = Button(text='    +    ', height=2, bg='#B8D41D', fg='#515F0B')
         self.back_lbl = Label(bg='#9DB0FF', width=214, height=3)
-        self.back_lbl.grid(row=1, column=0, columnspan=11, sticky=NW)
-
-        self.name_lbl = Label(text='название доски', height=2, bg='#9DB0FF', fg='#03273F')
-        self.name_lbl.place(x=5, y=55)
-
+        self.name_lbl = Label(text=self.name, height=2, bg='#9DB0FF', fg='#03273F')
         self.delete_board_bttn = Button(text='Удалить доску', height=2, bg='#F08080', fg='#800000')
-        self.delete_board_bttn.place(x=220, y=55)
-
         self.rename_board_bttn = Button(text='Переименовать', height=2, bg='#B1D2E7', fg='#03273F')
-        self.rename_board_bttn.place(x=315, y=55)
-
         self.add_to_favour_bttn = Checkbutton(text='Избранное', height=2, bg='#B1D2E7', fg='#03273F')
-        self.add_to_favour_bttn.place(x=420, y=55)
-
         self.invite_bttn = Button(text='Пригласить', height=2, bg='#B1D2E7', fg='#03273F')
-        self.invite_bttn.place(x=1345, y=55)
-
         self.persons_bttn = Button(text='Участники', height=2, bg='#B1D2E7', fg='#03273F')
+
+    def create_board_widgets(self):
+        self.new_list_bttn.place(x=self.start_x, y=110)
+        self.back_lbl.grid(row=1, column=0, columnspan=11, sticky=NW)
+        self.name_lbl.place(x=5, y=55)
+        self.delete_board_bttn.place(x=220, y=55)
+        self.rename_board_bttn.place(x=315, y=55)
+        self.add_to_favour_bttn.place(x=420, y=55)
+        self.invite_bttn.place(x=1345, y=55)
         self.persons_bttn.place(x=1427, y=55)
 
     def rename_board(self):
-        self.name = input('Введите новое название для доски:')
+        self.name = input('Введите название доски (например, Новая доска)')
+
+    def create_column(self):
+        def dismiss(window):
+            window.grab_release()
+            window.destroy()
+
+        def new_window():
+            window_assign_name = Toplevel()
+            window_assign_name.title("Введите название столбца (например, Новый столбец)")
+            window_assign_name.geometry("250x100+" + str(self.start_x) + "+150")
+            window_assign_name.resizable(False, False)
+            window_assign_name.protocol("WM_DELETE_WINDOW",
+                                        lambda: dismiss(window_assign_name))  # перехватываем нажатие на крестик
+
+            write_column_name = Entry(window_assign_name, width=40)
+            write_column_name.pack(anchor='nw', pady=[10, 10])
+
+            name = write_column_name.get()
+
+            get_name = Button(window_assign_name, text='Готово', command=lambda: dismiss(window_assign_name))
+            get_name.pack(anchor='center')
+            window_assign_name.grab_set()  # захватываем пользовательский ввод
+            return name
+
+        column_name = new_window()
--- main.py
+++ main.py
@@ -1,19 +1,77 @@
-from tkinter import *
-import customtkinter
-from classes import Application, Back
+from cl_tasktracker import *
+
+
+class Authorization(Frame):
+    def __init__(self, master):
+        super(Authorization, self).__init__(master)
+        self.grid()
+        self.create_widgets()
+
+    def create_widgets(self):
+        self.back_lbl = Label(bg='#8FA0E8', width=38, height=4)
+        self.back_lbl.grid(row=0, column=0, columnspan=3)
+
+        self.back_lbl = Label(bg='#9DB0FF', width=14, height=6)
+        self.back_lbl.grid(row=1, column=0, rowspan=2)
+
+        self.authorization_lbl = Label(text='АВТОРИЗАЦИЯ', font='15', width=14, height=2, bg='#9DB0FF', fg='#03273F')
+        self.authorization_lbl.grid(row=0, column=0, columnspan=3)
+
+        self.log_lbl = Label(text="Логин:", width=11, height=2, bg='#9DB0FF', fg='#03273F')
+        self.log_lbl.grid(row=1, column=0)
+
+        self.pas_lbl = Label(text="Пароль:", width=11, height=2, bg='#9DB0FF', fg='#03273F')
+        self.pas_lbl.grid(row=2, column=0)
+
+        self.login_ent = Entry(width=25, fg='#000000')
+        self.login_ent.grid(row=1, column=1)
+
+        self.password_ent = Entry(width=25, fg='#000000', show='*')
+        self.password_ent.grid(row=2, column=1)
+
+        self.entry_bttn = Button(text="ВХОД", font='15', width=15, height=1, bg='#B1D2E7', fg='#03273F',
+                                 command=self.check_log_pas)
+        self.entry_bttn.grid(row=3, column=0, columnspan=3)
+
+        self.error_lbl = Label(bg='#E0E0E0', fg='#FF0000')
+        self.error_lbl.grid(row=4, column=0, columnspan=3)
+
+    def check_log_pas(self):
+        if (self.login_ent.get() == '') or (self.password_ent.get() == ''):
+            self.error_lbl.config(text='Вы не ввели логин или пароль!')
+
+        elif (len(self.login_ent.get()) < 6) or \
+                (len(self.login_ent.get()) > 20) or \
+                (len(self.password_ent.get()) < 6) or \
+                (len(self.password_ent.get()) > 20):
+            self.error_lbl.config(text='Неверный логин или пароль')
+
+        else:
+            window.destroy()
+
+            root = Tk()
+            root.title('Axello')
+            root.geometry('1500x900+10+10')
+            root.resizable(False, False)
+            root.config(bg='#E0E0E0')
+
+            # создание рамки для размещения элементов
+            app = TaskTracker(root)
+
+            # старт событийного цикла
+            root.mainloop()
+
 
 if __name__ == '__main__':
-
     # создание базового окна
-    root = Tk()
-    root.title('Axello')
-    root.geometry('1500x900+10+10')
-    root.resizable(False, False)
+    window = Tk()
+    window.title('Вход')
+    window.geometry('272x230+600+300')
+    window.resizable(False, False)
+    window.config(bg='#E0E0E0')
 
     # создание рамки для размещения элементов
-    app = Back(root)
-    app = Application(root)
-    root.config(bg='#E0E0E0')
+    aut = Authorization(window)
 
     # старт событийного цикла
-    root.mainloop()
+    window.mainloop()
